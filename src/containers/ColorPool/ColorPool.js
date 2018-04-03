7aef3ce457ab6f2ad699442439261a3d4ca5e6dc
seiki tsunoda <liquidohr@gmail.com>
2018-04-02 21:48:11 -0700

    Render ColorPool #1

--- src/containers/ColorPool/ColorPool.js
+++ src/containers/ColorPool/ColorPool.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 
+import classes from './ColorPool.css'
+
 class ColorPool extends Component {
     render() {
-        return <div>Test</div>
+        return <div className={classes.Container}>
+            <div className={classes.Border}></div>
+            <div className={classes.ColorPool}>
+                <div className={classes.LeftBox}></div>
+                <div className={classes.RightBox}></div>
+            </div>
+        </div>
     }
 }
 
