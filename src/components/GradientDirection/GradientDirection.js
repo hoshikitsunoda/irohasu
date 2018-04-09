7034a536272f22be1c73ae0f1a52504918aeff43
seiki tsunoda <liquidohr@gmail.com>
2018-04-08 18:55:35 -0700

    Set ID for each list #17

--- src/components/GradientDirection/GradientDirection.js
+++ src/components/GradientDirection/GradientDirection.js
@@ -3,9 +3,23 @@ import React, { Component } from 'react'
 import classes from './GradientDirection.css'
 
 class GradientDirection extends Component {
+    directionHandler() {
+        const $backGroundLeft = document.querySelector('#leftText').innerHTML
+        const $backGroundRight = document.querySelector('#rightText').innerHTML
+        const $select = document.querySelector('#select').value
+
+        document.querySelector('#gradient').style.background = 
+            "linear-gradient(" + $select + ", " + $backGroundLeft + ", " + $backGroundRight + ")"
+        document.querySelector('#gradientText').innerHTML = 
+            "linear-gradient(" + $select + ", " + $backGroundLeft + ", " + $backGroundRight + ")"
+    }
+
     render() {
         return <div className={classes.DropDownWrapper}>
-            <select className={classes.DropDown}>
+            <select 
+                onChange={this.directionHandler}
+                id="select" 
+                className={classes.DropDown}>
                 <option id="toRight">to right</option>
                 <option id="toLeft">to left</option>
                 <option id="toBottom">to bottom</option>
