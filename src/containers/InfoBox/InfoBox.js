1d742e9fa88af38c2dab8496b43337ad6d64f56f
seiki tsunoda <liquidohr@gmail.com>
2018-04-08 21:33:18 -0700

    Add function to render color info for each color #19

--- src/containers/InfoBox/InfoBox.js
+++ src/containers/InfoBox/InfoBox.js
@@ -17,7 +17,14 @@ class InfoBox extends Component {
         const $rightHex = document.querySelector('#rightText').innerHTML.substr(1)
         axios.get('http://www.thecolorapi.com/id?hex=' + $rightHex)
                 .then(res => {
-                    console.log(res)
+                    this.setState({
+                        name: res.data.name.value,
+                        rgb: res.data.rgb.value,
+                        cmyk: res.data.cmyk.value,
+                        hsl: res.data.hsl.value,
+                        hsv: res.data.hsv.value,
+                        xyz: res.data.XYZ.value
+                    })
                 })
     }
 
@@ -27,6 +34,14 @@ class InfoBox extends Component {
                 id="dataHolder"
                 className={classes.InfoBox}
                 onMouseEnter={this.infoDataHandler}>
+                <ul>
+                    <li>color name: {this.state.name}</li>
+                    <li>rgb: {this.state.rgb}</li>
+                    <li>cmyk: {this.state.cmyk}</li>
+                    <li>hsl: {this.state.hsl}</li>
+                    <li>hsv: {this.state.hsv}</li>
+                    <li>xyz: {this.state.xyz}</li>
+                </ul>    
             </div>
         )
     }
